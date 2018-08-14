45ed88f8751a32f117ca3f89ea19c893831d41f1
bigwudan <bigwudan@qq.com>
2018-08-14 17:49:39 +0800

    Create shttpd_worker.c

---
+++ shttpd/shttpd_worker.c
@@ -0,0 +1,109 @@
+#include "shttpd.h"
+static struct worker_ctl *ctls = NULL;
+pthread_mutex_t thread_init = PTHREAD_MUTEX_INITIALIZER;
+
+
+static void *worker(void *arg)
+{
+	return NULL;
+}
+
+
+static void Worker_add(int i)
+{
+	pthread_t th;
+	pthread_mutex_lock(&thread_init);
+	int err = pthread_create(&th, NULL, worker, (void*)&ctls[i]);/*�����߳�*/
+	pthread_mutex_unlock(&thread_init);
+}
+
+
+static Worker_Init()
+{
+	ctls = (struct worker_ctl *)malloc(  my_conf_opts.MaxClient * sizeof( struct worker_ctl ) );
+
+	if(!ctls){
+		perror("malloc error\n");
+		exit(1);
+	}
+	memset(ctls, 0 , sizeof(ctls)*my_conf_opts.MaxClient  );
+	int n = 0;
+	for(n=0; n < my_conf_opts.MaxClient; n++){
+		ctls[n].opts.ctl = &ctls[n];
+		ctls[n].conn.ctl = &ctls[n];
+		ctls[n].opts.flag = 0;
+		pthread_mutex_init(&ctls[n].opts.mutex, NULL);
+		pthread_mutex_lock(&ctls[n].opts.mutex);
+	}
+	
+	for(n=0; n < my_conf_opts.MaxClient; n ++){
+		Worker_add(n);
+	}
+}
+
+
+int Worker_ScheduleRun(int ss)
+{
+	Worker_Init();
+	struct sockaddr_in client;
+	socklen_t len = sizeof(client);
+	int i = 0;
+	while(1){
+
+		printf("select start\n");
+
+		struct timeval tv;
+		fd_set rfds;
+		int retval = -1;
+		FD_ZERO(&rfds);
+		FD_SET(ss, &rfds);
+		tv.tv_sec = 0;
+		tv.tv_usec = 500000;
+		retval = select(ss+1, &rfds, NULL, NULL, &tv);
+		switch(retval){
+
+			case -1:
+				perror("select error");
+				exit;
+			case 0:
+				continue;
+			default:
+				if(FD_ISSET( ss, &rfds ) ){
+					int sc = accept(ss, (struct sockaddr*)&client, &len  );	
+						
+					printf("sc=%d\n", sc);
+					close(sc);
+				
+				
+				}
+				
+				
+		
+		
+		
+		
+		
+		
+		
+		}	
+			
+
+
+
+
+
+
+
+	
+	
+	}
+
+	
+
+
+}
+
+
+
+
+
