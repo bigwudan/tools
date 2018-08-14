dbe1abfa1005df74f5b4e45c6342d48ceb393c70
bigwudan <bigwudan@qq.com>
2018-08-14 17:49:09 +0800

    Create shttpd.h

---
+++ shttpd/shttpd.h
@@ -0,0 +1,77 @@
+#ifndef _SHTTPD_H_
+#define _SHTTPD_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+
+
+
+struct conf_opts{
+	char CGIRoot[128];		/*CGI��·��*/
+	char DefaultFile[128];		/*Ĭ���ļ�����*/
+	char DocumentRoot[128];	/*���ļ�·��*/
+	char ConfigFile[128];		/*�����ļ�·��������*/
+	int ListenPort;			/*�����˿�*/
+	int MaxClient;			/*���ͻ�������*/
+	int TimeOut;				/*��ʱʱ��*/
+	int InitClient;				/*��ʼ���߳�����*/
+};
+
+extern struct conf_opts my_conf_opts;
+
+
+struct worker_ctl;
+struct worker_opts
+{
+	pthread_t th;
+	int flag;
+	pthread_mutex_t mutex;
+	struct worker_ctl *ctl;
+};
+
+
+struct worker_conn
+{
+#define K 1024
+	char drep[16*K];	
+	char dres[16*K];
+	int cs;
+	int to;
+	struct worker_ctl *ctl;
+
+
+};
+
+
+struct worker_ctl
+{
+	struct worker_opts opts;
+	struct worker_conn conn;
+};
+
+extern int Worker_ScheduleRun(int);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
