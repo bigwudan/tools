f6a71f185e81f934855c4eaf81ae04ff7b04f082
bigwudan <bigwudan@qq.com>
2018-09-08 08:01:37 +0800

    111

--- ngx_tool/main.c
+++ ngx_tool/main.c
@@ -3,6 +3,31 @@
 #include <string.h>
 #include "ngx_tool.h"
 
+
+int create_pool(ngx_pool_t *p_ngx_pool_t, int size)
+{
+	char *p_data = NULL;
+	posix_memalign(&p_data, 16, size);
+	p_ngx_pool_t = calloc(sizeof(ngx_pool_t), 1);
+
+	p_ngx_pool_t->max = size;
+	p_ngx_pool_t->large = NULL;
+	p_ngx_pool_t->cleanup = NULL;
+	p_ngx_pool_t->current = p_ngx_pool_t;
+	p_ngx_pool_t->d->last = p_data;
+	p_ngx_pool_t->d->end = p_data+size;
+	p_ngx_pool_t->d->next = 0;
+	p_ngx_pool_t->d->failed = 0;
+
+
+
+
+	
+}
+
+
+
+
 int main()
 {
 	printf("test\n");
