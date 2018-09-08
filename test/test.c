28a3394602057d677cf1b387d949d7f5cd1586b4
bigwudan <bigwudan@qq.com>
2018-09-08 08:02:34 +0800

    111

--- test/test.c
+++ test/test.c
@@ -4,51 +4,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <pthread.h>
-
-void *pthread_run1(void *arg)
-{
-    (void)arg;
-    while(1){
-        printf("I am thread1, ID:%lu\n", pthread_self());
-        
-        printf("test\n");
-
-        sleep(2);    
-    
-    }
-
-
-
-}
-
-
-void* pthread_run2(void* arg)
-{
-    (void)arg;
-    while(1)
-    {
-        printf("I am thread2,ID: %lu\n",pthread_self());
-        sleep(2);
-    }
-}
+#define NGX_ALIGNMENT   sizeof(unsigned long)    /* platform word */
 
 int main()
 {
-    pthread_t tid1;
-    pthread_t tid2;
 
-    printf("I am main thread\n");
-
-    pthread_create(&tid2, NULL, pthread_run2, NULL);
-    pthread_create(&tid1, NULL, pthread_run1, NULL);
-
-    pthread_join(tid1, NULL);
-    pthread_join(tid2, NULL);
-
-
-
+	printf("len=%d\n", NGX_ALIGNMENT);
 
 }
-
-
-
