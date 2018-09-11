7be0ea4297bfbb71fd13b366463777cbfb8a1d06
bigwudan <bigwudan@qq.com>
2018-09-11 16:13:01 +0800

    111

--- test/test.c
+++ test/test.c
@@ -1,35 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-
-
-
-int main()
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+ 
+int main(int argc,char**argv)
 {
-    char *url = "http://www.192.167.1.103:83421103333/";
+    int pipe_fd[2];
+    pid_t pid;
+    char r_buf[4096];
+    char w_buf[4096 * 2];
+    int writenum;
+    int rnum;
 
-    char *p_beg = strstr(url+7, ":");
-    char *p_end = strstr(url+7, "/");
-    p_beg = p_beg+1;
-    int i = p_end - p_beg;
-        
-    char buf[100] = {0};
+    memset(r_buf, 0, sizeof(r_buf));
 
-    strncpy(buf, p_beg, i);
-    buf[i] = '\0';
-        
-    int host = 0;
-    host = atoi(buf);
+    if (pipe(pipe_fd) < 0)
+    {
+        printf("pipe create error\n");
+        return -1;
+    }
 
-
-    printf("host=%d\n", host);
-
-    printf("i=%d\n",i);
-
-    printf("test\n");
-
+    if ((pid = fork()) == 0)
+    {
+        close(pipe_fd[1]);
+        while (1)
+        {
+            sleep(1);
+            rnum = read(pipe_fd[0], r_buf, 1000);
+            printf("child: readnum is %d\n", rnum);
+        }
+        close(pipe_fd[0]);
+        exit(1);
+    }
+    else if (pid > 0)
+    {
+        close(pipe_fd[0]);//write
+        memset(r_buf, 0, sizeof(r_buf));
+        if ((writenum = write(pipe_fd[1], w_buf, 1024)) == -1)
+        {
+            printf("write to pipe error\n");
+        }
+        else
+        {
+            printf("the bytes write to pipe is %d \n", writenum);
+        }
+        writenum = write(pipe_fd[1], w_buf, 4096);
+        close(pipe_fd[1]);
+    }
 }
-
-
-
