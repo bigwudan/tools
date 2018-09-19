f22e7fd32943448244fefc595b87832a9d68a324
bigwudan <bigwudan@qq.com>
2018-09-19 18:06:27 +0800

    111

--- test/test.c
+++ test/test.c
@@ -1,25 +1,50 @@
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/epoll.h>
 
-int main(int argc, char *argv[])
+
+enum HTTP_CODE { NO_REQUEST, GET_REQUEST, BAD_REQUEST, FORBIDDEN_REQUEST, INTERNAL_ERROR, CLOSED_CONNECTION };
+
+char *p_str = "GET http://www.baidu.com/index.html HTTP/1.0";
+
+int main(int argc, char* argv[])
 {
-    int epfd, nfds;
-    struct epoll_event ev, events[5];
-    epfd =epoll_create(1);
-    ev.data.fd = STDIN_FILENO;
-    ev.events =EPOLLIN;
-    epoll_ctl(epfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev);
-    for(;;)
+    
+    char *szTemp = calloc(strlen(p_str), 1);    
+
+    strcpy(szTemp, p_str);
+
+
+
+    char* szURL = strpbrk( szTemp, " \t" );
+    if ( ! szURL )
     {
-        nfds = epoll_wait(epfd, events, 5, -1);
-        for(int i =0; i <nfds; i++){
-            if(events[i].data.fd == STDIN_FILENO){
-                char buf[1024] = {0};
-                read( events[i].data.fd,buf, sizeof(buf));
-                printf("welcome to epoll's word!\n");
-            }
-        }
+        return BAD_REQUEST;
     }
-    return 1;
+    *szURL++ = '\0';
+
+    char* szMethod = szTemp;
+    if ( strcasecmp( szMethod, "GET" ) == 0 )
+    {
+        printf( "The request method is GET\n" );
+    }
+    else
+    {
+        return BAD_REQUEST;
+    }
+    
+    //szURL += strspn( szURL, " \t" );
+
+    printf("len=%lu\n", strspn(szURL, " \t"));
+
+
+
+
+    printf("test\n");
+
+
 }
+
+
+
