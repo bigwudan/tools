260d75d0a492e999c770d75feba2a925a59bf8f5
bigwudan <bigwudan@qq.com>
2018-10-13 15:38:12 +0800

    111

--- test/test.c
+++ test/test.c
@@ -10,118 +10,52 @@
 #include <errno.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define IP   "127.0.0.1"
 #define PORT  8888
 #define PROCESS_NUM 4
 #define MAXEVENTS 64
-
-static int create_and_bind ()
+int stop = 1;
+void handle_term(int sig)
 {
-    int fd = socket(PF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in serveraddr;
-    serveraddr.sin_family = AF_INET;
-    inet_pton( AF_INET, IP, &serveraddr.sin_addr);  
-    serveraddr.sin_port = htons(PORT);
-    bind(fd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
-    return fd;
-}
+	stop = 0;
 
-static int make_socket_non_blocking (int sfd)
-{
-    int flags, s;
-    flags = fcntl (sfd, F_GETFL, 0);
-    if (flags == -1) {
-        perror ("fcntl");
-        return -1;
-    }
-    flags |= O_NONBLOCK;
-    s = fcntl (sfd, F_SETFL, flags);
-    if (s == -1) {
-        perror ("fcntl");
-        return -1;
-    }
-    return 0;
-}
-
-void worker(int sfd, int efd, struct epoll_event *events, int k) {
-    /* The event loop */
-    while (1) {
-        int n, i;
-        n = epoll_wait(efd, events, MAXEVENTS, -1);
-        sleep(2);
-        printf("worker  %d return from epoll_wait!\n", k);
-        for (i = 0; i < n; i++) {
-            if ((events[i].events & EPOLLERR) || (events[i].events & EPOLLHUP) || (!(events[i].events &EPOLLIN))) {
-                /* An error has occured on this fd, or the socket is not ready for reading (why were we notified then?) */
-                fprintf (stderr, "epoll error\n");
-                close (events[i].data.fd);
-                continue;
-            } else if (sfd == events[i].data.fd) {
-                /* We have a notification on the listening socket, which means one or more incoming connections. */
-                struct sockaddr in_addr;
-                socklen_t in_len;
-                int infd;
-                char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
-                in_len = sizeof in_addr;
-                infd = accept(sfd, &in_addr, &in_len);
-                if (infd == -1) {
-                    printf("worker %d accept failed!\n", k);
-                    break;
-                }
-                printf("worker %d accept successed!\n", k);
-                /* Make the incoming socket non-blocking and add it to the list of fds to monitor. */
-                close(infd); 
-            }
-        }
-    }
 }
 
 int main (int argc, char *argv[])
 {
-    int sfd, s;
-    int efd;
-    struct epoll_event event;
-    struct epoll_event *events;
-    sfd = create_and_bind();
-    if (sfd == -1) {
-        abort ();
-    }
-    s = make_socket_non_blocking (sfd);
-    if (s == -1) {
-        abort ();
-    }
-    s = listen(sfd, SOMAXCONN);
-    if (s == -1) {
-        perror ("listen");
-        abort ();
-    }
-    efd = epoll_create(MAXEVENTS);
-    if (efd == -1) {
-        perror("epoll_create");
-        abort();
-    }
-    event.data.fd = sfd;
-    event.events = EPOLLIN;
-    s = epoll_ctl(efd, EPOLL_CTL_ADD, sfd, &event);
-    if (s == -1) {
-        perror("epoll_ctl");
-        abort();
-    }
-
-    /* Buffer where events are returned */
-    events = calloc(MAXEVENTS, sizeof event);
-    int k;
-    for(k = 0; k < PROCESS_NUM; k++) {
-        printf("Create worker %d\n", k+1);
-        int pid = fork();
-        if(pid == 0) {
-            worker(sfd, efd, events, k);
-        }
-    }
-    int status;
-    wait(&status);
-    free (events);
-    close (sfd);
-    return EXIT_SUCCESS;
+	int fd = socket(PF_INET, SOCK_STREAM, 0);
+	int ret;
+	if(fd == -1){
+		perror("socket error\n");
+		exit(1);
+	}
+	struct sockaddr_in serveraddr;
+	serveraddr.sin_family = AF_INET;
+	inet_pton( AF_INET, IP, &serveraddr.sin_addr);  
+	serveraddr.sin_port = htons(PORT);
+	serveraddr.sin_addr.s_addr = INADDR_ANY;
+	ret = bind(fd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if(ret == -1){
+		perror("bind error\n");
+	}
+	ret = listen(fd, 1);
+	if(ret == -1) perror("listen error\n");
+	char buf[120] = {0};
+	int conn = 0;
+	while(stop){
+	
+		memset(buf, 0, 120);
+		conn = accept(fd, NULL, NULL);
+		if(conn > 0 ){
+			printf("ok conn\n");
+		}
+		
+		
+		sleep(1);
+	}
+	close(fd);
+	return 0;
 }
