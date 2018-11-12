bec3031e197955da5d47bf1b1097dc2a0e0949ef
bigwudan <bigwudan@qq.com>
2018-11-12 13:36:22 +0800

    111

---
+++ concurrency_client/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define PROCESS_NUM 1
+#define THREAD_NUM 10
+#define EXPIRED_TIME 30
+#define THREAD_TASK_NUM 30
+
+#endif
---
+++ concurrency_client/test_main.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+
+
+
+
+#include "config.h"
+
+
+int main(int argc, char **argv)
+{
+
+    printf("test\n");
+}
