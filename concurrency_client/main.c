0fc5d71ff4287963792f1a77abea4c8dcd183bd1
bigwudan <bigwudan@qq.com>
2018-11-19 11:05:47 +0800

    11

--- concurrency_client/main.c
+++ concurrency_client/main.c
@@ -4,7 +4,14 @@
 
 #include "parse_header_fsm.h"
 
+char p_m_buf[] = "HTTP/1.1 200 OK\r\nDate: Mon, 19 Nov 2018 01:01:37 GM\r\nServer: Apache/2.4.6 (CentOS) PHP/5.4.16\r\nX-Powered-By: PHP/5.4.16\r\nContent-Length: 30\r\nContent-Type: text/html; charset=UTF-8\r\n\r\nint(1542589297)string(1)";
+
+
 int main(){
+    
+    printf("p=%s\n", p_m_buf); 
+
+    return 1;
 	char p_buf[] = "1234567\r\nabcd\r\ncd";
 	int checked = 0;
 	int readed = strlen(p_buf);
--- concurrency_client/parse_header_fsm.h
+++ concurrency_client/parse_header_fsm.h
@@ -2,14 +2,26 @@
 #define PARSE_HEADER_FSM_H
 
 enum LINE_STATE{LINE_OK, LINE_ING, LINE_BAD  };
-
+enum HEAD_VAL_TYPE { STRING, INT };
 
 extern enum LINE_STATE
 parse_line(char *p_buf, int *p_checked, int *p_readed, int *p_begline);
 
 
+union head_value_variant{
+    int i_val;
+    char *p_c_val;
+};
+
+struct head_param{
+    char *p_name;
+    enum HEAD_VAL_TYPE val_type;
+    union head_value_variant val;
+    int len;
+};
 
 
+extern struct head_param head_param_list[]; 
 
 
 
