b9c3fa7ad013c16553b1be76ade78209ccdf87c7
bigwudan <bigwudan@qq.com>
2019-09-07 06:33:34 -0700

    11

--- http_buffer/buffer.c
+++ http_buffer/buffer.c
@@ -1,3 +1,9 @@
+#include <stdarg.h>  
+#include <stddef.h>  
+#include <setjmp.h>  
+#include <google/cmockery.h>  
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,7 +12,7 @@
 
 
 
-#define ARRAY_IDX 16
+#define ARRAY_IDX 2
 
 
 extern char *strdup(const char *s);
@@ -17,6 +23,24 @@ typedef struct{
     size_t used; //已经使用的长度
     size_t size;//总长度
 }buff_array;
+
+//查找
+char *
+buff_find_key(const buff_array *p_buff, const char *key, int *idx)
+{
+    size_t ndata = 0;
+    char *tmp = NULL;
+    for(int i =0; i < p_buff->used; i++){
+        ndata = p_buff->sort[i];
+        tmp = p_buff->data[ndata];
+        if(strcmp(tmp, key) == 0){
+            *idx = i;
+            return tmp;
+        }
+    }
+    return NULL;
+}
+
 
 //初始化
 buff_array *
@@ -59,14 +83,13 @@ buff_insert(buff_array *p_arr, char *key )
             return -1;
         } 
         p_arr->sort = realloc(p_arr->sort, (p_arr->size + ARRAY_IDX)*(sizeof(size_t)));
-    
+        p_arr->size = p_arr->size + ARRAY_IDX;    
     }
 
     int need_idx= 0;
     //查找小于他的数
     for(int i=0; i < p_arr->used; i++){
         idx =  p_arr->sort[i]; 
-        p_arr->data[idx];
         flag = strcmp(p_arr->data[idx], key);
         if(flag == 0){
             return 0;
@@ -80,29 +103,122 @@ buff_insert(buff_array *p_arr, char *key )
     
     //如果是末尾直接插入
     if(need_idx== 0){
-        p_arr->sort[p_arr->used - 1] = p_arr->used - 1;
-        p_arr->data[p_arr->used - 1] = strdup(key);
+        p_arr->sort[p_arr->used] = p_arr->used;
+        p_arr->data[p_arr->used] = strdup(key);
         p_arr->used++; 
         return 0;
     }
-
     //中间插入
     int remain_len = p_arr->used - need_idx;
-    memmove(p_arr->sort+need_idx+1, p_arr->sort+need_idx, remain_len);
+    memmove(p_arr->sort+need_idx+1, p_arr->sort+need_idx, remain_len*sizeof(size_t));
     p_arr->data[p_arr->used] = strdup(key);
     p_arr->sort[need_idx] = p_arr->used;
     p_arr->used++;
     return 0;
 }
 
+//打印数据
+void
+buff_printf(buff_array *p_buf)
+{
+    for(int i=0; i<p_buf->used; i++){
+        printf("i=%d, sort=%ld , str=%s\n", i, p_buf->sort[i], p_buf->data[i]);
+
+
+    }
+
+
+
+}
+
+//删除key
+int
+buff_del_key(buff_array *p_buff, char *key)
+{
+    int idx = 0;
+    int data_idx = 0;
+    char *tmp_key = NULL;
+    tmp_key = buff_find_key(p_buff, key, &idx);
+    if(strcmp(tmp_key, key) == 0){
+        data_idx = p_buff->sort[idx];
+        //去掉data
+        free(p_buff->data[data_idx]);
+        memmove(p_buff->data+data_idx, p_buff->data+data_idx+1, (p_buff->used - (data_idx +1))*sizeof(char *)  );
+        //去掉sort
+        memmove(p_buff->sort+idx, p_buff->sort+idx+1, (p_buff->used - (idx+1))*sizeof(size_t) ) ;
+        p_buff->used--;
+        return 0;
+    }else{
+        return 1;
+    }
+
+
+}
+
+
+void test_sub(void **state) {  
+    int flag = 0;
+    buff_array *p_buf = buff_init();
+    assert_true(p_buf);            
+
+    //第一个测试用例
+    char *test1 = "12";
+    buff_insert(p_buf, test1);
+    assert_int_equal(p_buf->used, 1);
+    assert_string_equal(p_buf->data[0], test1);
+    assert_int_equal(p_buf->sort[0], 0);
+    
+    //第二个测试用例
+    char *test2 = "34";
+    buff_insert(p_buf, test2);
+    assert_int_equal(p_buf->used, 2);
+    assert_string_equal(p_buf->data[1], test2);
+    assert_int_equal(p_buf->sort[1], 1);
+    //第二个测试用例
+    char *test3 = "56";
+    buff_insert(p_buf, test3);
+    assert_int_equal(p_buf->used, 3);
+    assert_string_equal(p_buf->data[2], test3);
+    assert_int_equal(p_buf->sort[2], 2);
+    //第二个测试用例
+    char *test4 = "78";
+    buff_insert(p_buf, test4);
+    assert_int_equal(p_buf->used, 4);
+    assert_string_equal(p_buf->data[3], test4);
+    assert_int_equal(p_buf->sort[3], 3);
+
+
+    //第二个测试用例
+    char *test5 = "35";
+    buff_insert(p_buf, test5);
+    
+    //del
+    flag = buff_del_key(p_buf, "34");
+    printf("flag = %d\n", flag);
+    return ;
+
+    //查找
+    char *tmp_key = NULL;
+    int tmp_idx= 0;
+    tmp_key = buff_find_key(p_buf, "34", &tmp_idx);
+
+    assert_string_equal(tmp_key, "34");
+    assert_int_equal(tmp_idx, 1);
+
+    //打印数据
+    buff_printf(p_buf);
+
+    printf("test finish\n");
+}  
 
 int main()
 {
-    buff_array *p_buf = buff_init();
+    const UnitTest tests[] = {  
+        unit_test(test_sub)  
 
-    printf("wudan\n");
+    };  
+    return run_tests(tests);  
 
-    return 0;
 }
 
 
