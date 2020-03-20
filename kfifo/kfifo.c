8a6b5fa66088466978cc5382e5a6ca69a2df9cea
bigwudan <bigwudan@qq.com>
2020-03-20 21:24:22 +0800

    1

--- kfifo/kfifo.c
+++ kfifo/kfifo.c
@@ -6,139 +6,127 @@
 
 #include "kfifo.h"
 
-static inline unsigned int kfifo_unused(struct __kfifo *fifo)
+static inline int fls(int x)
 {
-        return (fifo->mask + 1) - (fifo->in - fifo->out);
+	int position;
+	int i;
+	if(0 != x)
+	{
+		for (i = (x >> 1), position = 0; i != 0; ++position)
+			i >>= 1;
+	}
+	else
+	{
+		position = -1;
+	} 
+	return position+1;
 }
 
-uint32_t roundup_pow_of_two(const uint32_t x)
+static inline unsigned int roundup_pow_of_two(unsigned int x)
 {
-    if (x == 0){ return 0; }
-    if (x == 1){ return 2; }
-    uint32_t ret = 1;
-    while (ret < x)
-    {
-        ret = ret << 1;
-    }
-    return ret;
-}
-
-
-
-
-int __kfifo_alloc(struct __kfifo *fifo, unsigned int size,
-                size_t esize)
-{
-    /*
-     *   * round up to the next power of 2, since our 'let the indices
-     *       * wrap' technique works only in this case.
-     *           */
-    size = roundup_pow_of_two(size);
-
-    fifo->in = 0;
-    fifo->out = 0;
-    fifo->esize = esize;
-
-    if (size < 2) {
-        fifo->data = NULL;
-        fifo->mask = 0;
-        return -1;
-    }
-
-    fifo->data = malloc(size*esize);
-
-    if (!fifo->data) {
-        fifo->mask = 0;
-        return -1;
-    }
-    fifo->mask = size - 1;
-
-    return 0;
-}
-
-
-int __kfifo_init(struct __kfifo *fifo, void *buffer,
-        unsigned int size, size_t esize)
-{
-    size /= esize;
-
-    size = roundup_pow_of_two(size);
-
-    fifo->in = 0;
-    fifo->out = 0;
-    fifo->esize = esize;
-    fifo->data = buffer;
-
-    if (size < 2) {
-        fifo->mask = 0;
-        return -1;
-    }
-    fifo->mask = size - 1;
-
-    return 0;
-}
-
-void __kfifo_free(struct __kfifo *fifo)
-{
-    free(fifo->data);
-    fifo->in = 0;
-    fifo->out = 0;
-    fifo->esize = 0;
-    fifo->data = NULL;
-    fifo->mask = 0;
-}
-
-
-static unsigned int
-min(int a, int b)
-{
-    if(a > b){
-        return b;
-    }else{
-        return a;
-    }
-}
-
-
-static void kfifo_copy_in(struct __kfifo *fifo, const void *src,
-                unsigned int len, unsigned int off)
-{
-    unsigned int size = fifo->mask + 1;
-    unsigned int esize = fifo->esize;
-    unsigned int l;
-
-    off &= fifo->mask;
-    if (esize != 1) {
-        off *= esize;
-        size *= esize;
-        len *= esize;
-    }
-    l = min(len, size - off);
-
-    memcpy(fifo->data + off, src, l);
-    memcpy(fifo->data, src + l, len - l);
-
-
-}
-
-unsigned int __kfifo_in(struct __kfifo *fifo,
-        const void *buf, unsigned int len)
-{
-    unsigned int l;
-
-    l = kfifo_unused(fifo);
-    if (len > l)
-        len = l;
-
-    kfifo_copy_in(fifo, buf, len, fifo->in);
-    fifo->in += len;
-    return len;
+	return 1UL << fls(x - 1);
 }
 
 
 
+/* 创建队列 */
+struct kfifo *kfifo_init(unsigned char *buffer, unsigned int size)
+{
+	struct kfifo *fifo;
+	/* size must be a power of 2 ：判断是否为2的幂*/
+	fifo = malloc(sizeof(struct kfifo));
+	if (!fifo)
+		return NULL;
+	fifo->buffer = buffer;
+	fifo->size = size;
+	fifo->in = fifo->out = 0;
+	return fifo;
+}
 
+/* 分配空间 */
+struct kfifo *kfifo_alloc(unsigned int size)
+{
+	unsigned char *buffer;
+	struct kfifo *ret;
+	if (!is_power_of_2(size)) {  /* 判断是否为2的幂 */
+		size = roundup_pow_of_two(size); /* 如果不是则向上扩展成2的幂 */
+	}
+	buffer = malloc(size);
+	if (!buffer)
+		return NULL;
+	ret = kfifo_init(buffer, size);
+	if(ret == NULL){
+		free(buffer);	
+	}	 
 
+	return ret;
+}
+
+unsigned int kfifo_put(struct kfifo *fifo,
+                 const unsigned char *buffer, unsigned int len)
+ {
+     unsigned int ret;
+     ret = __kfifo_put(fifo, buffer, len);
+     return ret;
+ }
+ 
+unsigned int kfifo_get(struct kfifo *fifo,
+                      unsigned char *buffer, unsigned int len)
+ {
+     unsigned int ret;
+     ret = __kfifo_get(fifo, buffer, len);
+     //当fifo->in == fifo->out时，buufer为空
+     if (fifo->in == fifo->out)
+         fifo->in = fifo->out = 0;
+     return ret;
+ }
+ 
+ unsigned int __kfifo_put(struct kfifo *fifo,
+             const unsigned char *buffer, unsigned int len)
+ {
+     unsigned int l;
+     //buffer中空的长度
+     len = min(len, fifo->size - fifo->in + fifo->out);
+     /*
+      * Ensure that we sample the fifo->out index -before- we
+      * start putting bytes into the kfifo.
+      */
+     /* first put the data starting from fifo->in to buffer end */
+     l = min(len, fifo->size - (fifo->in & (fifo->size - 1)));
+     memcpy(fifo->buffer + (fifo->in & (fifo->size - 1)), buffer, l);
+     /* then put the rest (if any) at the beginning of the buffer */
+     memcpy(fifo->buffer, buffer + l, len - l);
+ 
+     /*
+      * Ensure that we add the bytes to the kfifo -before-
+      * we update the fifo->in index.
+      */
+     fifo->in += len;  //每次累加，到达最大值后溢出，自动转为0
+     return len;
+ }
+ 
+ unsigned int __kfifo_get(struct kfifo *fifo,
+              unsigned char *buffer, unsigned int len)
+ {
+     unsigned int l;
+     //有数据的缓冲区的长度
+     len = min(len, fifo->in - fifo->out);
+     /*
+      * Ensure that we sample the fifo->in index -before- we
+      * start removing bytes from the kfifo.
+      */
+     /* first get the data from fifo->out until the end of the buffer */
+     l = min(len, fifo->size - (fifo->out & (fifo->size - 1)));
+     memcpy(buffer, fifo->buffer + (fifo->out & (fifo->size - 1)), l);
+     /* then get the rest (if any) from the beginning of the buffer */
+     memcpy(buffer + l, fifo->buffer, len - l);
+     /*
+      * Ensure that we remove the bytes from the kfifo -before-
+      * we update the fifo->out index.
+      */
+     fifo->out += len; //每次累加，到达最大值后溢出，自动转为0
+     return len;
+ }
 
 
 
--- kfifo/kfifo.h
+++ kfifo/kfifo.h
@@ -2,25 +2,44 @@
 #define _KFIFO_H_
 #include <stdlib.h>
 
+//判断x是否是2的次方  
+#define is_power_of_2(x) ((x) != 0 && (((x) & ((x) - 1)) == 0))  
+//取a和b中最小值  
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 
 
-struct __kfifo {
-    unsigned int    in;      // 入列的时候增加的位置
-    unsigned int    out;     // 出列的时候增加的位置
-    unsigned int    mask;    // 巧妙的 mask 设计，同时包含了数据的个数信息
-    unsigned int    esize;   // 元素的大小
-    void            *data;   // 数据
+struct kfifo {
+	unsigned char *buffer;    /* the buffer holding the data : 用于存放数据的缓存 */
+	unsigned int size;    /* the size of the allocated buffer : 空间的大小，在初化时将它向上扩展成2的幂，为了高效的进行与操作取余，后面会详解 */
+	unsigned int in;    /* data is added at offset (in % size) ： 如果使用不能保证任何时间最多只有一个读线程和写线程，需要使用该lock实施同步*/
+	unsigned int out;    /* data is extracted from off. (out % size) ：一起构成一个循环队列。 in指向buffer中队头，而且out指向buffer中的队尾 */
 };
 
 
-int __kfifo_alloc(struct __kfifo *fifo, unsigned int size, size_t esize);
+//根据给定buffer创建一个kfifo
+struct kfifo *kfifo_init(unsigned char *buffer, unsigned int size);
+
+//给定size分配buffer和kfifo
+struct kfifo *kfifo_alloc(unsigned int size);
+//释放kfifo空间
+void kfifo_free(struct kfifo *fifo);
+
+//向kfifo中添加数据
+unsigned int kfifo_put(struct kfifo *fifo,
+		const unsigned char *buffer, unsigned int len);
+
+//从kfifo中取数据
+unsigned int kfifo_get(struct kfifo *fifo,
+		unsigned char *buffer, unsigned int len);
+
+//获取kfifo中有数据的buffer大小 
+unsigned int kfifo_len(struct kfifo *fifo);
 
 
-int __kfifo_init(struct __kfifo *fifo, void *buffer, unsigned int size, size_t esize);
+unsigned int __kfifo_put(struct kfifo *fifo, const unsigned char *buffer, unsigned int len);
+
+unsigned int __kfifo_get(struct kfifo *fifo, unsigned char *buffer, unsigned int len);
 
 
-void __kfifo_free(struct __kfifo *fifo);
+#endif 
 
-unsigned int __kfifo_in(struct __kfifo *fifo, const void *buf, unsigned int len);
-
-#endif
