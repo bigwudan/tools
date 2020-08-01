548c5d6c8e26a1d2bd676e6cbe322c5c6c95baf6
bigwudan <bigwudan@qq.com>
2020-08-01 02:45:28 -0700

    11

---
+++ heap/heap_4.h
@@ -0,0 +1,32 @@
+#ifndef _HEAP_4_H
+#define _HEAP_4_H
+
+#include <stdlib.h>
+#include <stdint.h>
+#define portBYTE_ALIGNMENT  4
+
+#define configTOTAL_HEAP_SIZE 2000
+
+#define portBYTE_ALIGNMENT_MASK ( 0x0007 )
+
+#define vTaskSuspendAll()
+#define	xTaskResumeAll()
+#define mtCOVERAGE_TEST_MARKER()
+
+#define	traceMALLOC( A, B )
+
+#define vApplicationMallocFailedHook()
+
+#define configASSERT(A)
+#define traceFREE(A,B)
+
+
+void *pvPortMalloc( size_t xWantedSize );
+void vPortFree( void *pv );
+
+
+size_t xPortGetFreeHeapSize( void );
+
+size_t xPortGetMinimumEverFreeHeapSize( void );
+#endif
+
